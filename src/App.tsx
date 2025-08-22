6cda76847a4505955134bcc2f0afe84a23b029ef
eunice <emaestimada@gmail.com>
2025-08-22 17:05:58 +0800

    added tabs area and draft content for the homepage

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,12 @@ import {
   setupIonicReact
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { heart, helpCircle, home, person, searchCircle } from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
+import Tab4 from './pages/Tab4';
+import Tab5 from './pages/Tab5';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -51,32 +53,49 @@ const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonTabs>
+        {/* pang route sa tabs/pages */}
         <IonRouterOutlet>
           <Route exact path="/tab1">
-            <Tab1 />
-          </Route>
-          <Route exact path="/tab2">
-            <Tab2 />
-          </Route>
-          <Route path="/tab3">
-            <Tab3 />
-          </Route>
-          <Route exact path="/">
-            <Redirect to="/tab1" />
-          </Route>
+          <Tab1 />
+        </Route>
+        <Route exact path="/tab2">
+          <Tab2 />
+        </Route>
+        <Route exact path="/tab3">
+          <Tab3 />
+        </Route>
+        <Route exact path="/tab4">
+          <Tab4 />
+        </Route>
+        <Route exact path="/tab5">
+          <Tab5 />
+        </Route>
+        <Route exact path="/">
+          <Redirect to="/tab1" />
+        </Route>
         </IonRouterOutlet>
+
+        {/* tab bar */}
         <IonTabBar slot="bottom">
           <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon aria-hidden="true" icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
+            <IonIcon aria-hidden="true" icon={home} />
+            <IonLabel>Home</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon aria-hidden="true" icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
+            <IonIcon aria-hidden="true" icon={heart} />
+            <IonLabel>Donations</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon aria-hidden="true" icon={square} />
-            <IonLabel>Tab 3</IonLabel>
+            <IonIcon aria-hidden="true" icon={searchCircle} />
+            <IonLabel>Trace</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="tab4" href="/tab4">
+            <IonIcon aria-hidden="true" icon={person} />
+            <IonLabel>Profile</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="tab5" href="/tab5">
+            <IonIcon aria-hidden="true" icon={helpCircle} />
+            <IonLabel>Help</IonLabel>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,24 +1,32 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardContent } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
+  const userName = "Eunice"; // placeholder, will replace with API later
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+      <IonContent fullscreen className="home-content">
+        {/* Greeting Section */}
+        <div className="home-greeting">
+          <h1>Hello, <span className="user-name">{userName}</span> 👋</h1>
+        </div>
+        {/* <ExploreContainer name="Home page" /> */}
 
-        Hello World
+        {/* Quick Stats / Cards Example */}
+        <IonCard>
+          <IonCardContent>
+            <h2>Total Donations</h2>
+            <p>12 Items</p>
+          </IonCardContent>
+        </IonCard>
+
+        <IonCard>
+          <IonCardContent>
+            <h2>Items Delivered</h2>
+            <p>7 Items</p>
+          </IonCardContent>
+        </IonCard>
       </IonContent>
     </IonPage>
   );
